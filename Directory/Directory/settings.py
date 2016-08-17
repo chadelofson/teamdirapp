0ebcb3124bad2e4f9d2c339e382c59b89d6de4f2
Chad Elofson <chad.elofson@gmail.com>
2016-08-17 09:59:27 -0700

    corrected setting in setting.py file

--- Directory/Directory/settings.py
+++ Directory/Directory/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS (
+STATICFILES_DIRS = (
     BASE_DIR + '/www/static',
 )
 
